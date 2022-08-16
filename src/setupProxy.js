0426abfec69f610e29c683c3f324e4e62b3aee69
unknown <muskaan2lamba@gmail.com>
2022-08-16 23:35:37 +0530

    -changed target in proxy setup

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/feedback',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://feedback-json-server-react.herokuapp.com',
       changeOrigin: true,
     })
   );
